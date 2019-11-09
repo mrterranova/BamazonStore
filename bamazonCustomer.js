db6ae646b04cb095ef305ee5d065c9ed95b276e2
Michal Terranova <michalterranova@mail.com>
2019-11-08 19:51:02 -0500

    making console logs look entertaining

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,16 @@ let connection = mysql.createConnection({
 
 connection.connect(err => {
     if(err) throw err;
-    console.log("connected as id " + connection.threadID + "\n")
+    console.log(
+"#     #                                                              ######     #    #     #    #    ####### ####### #     #     ###\n"+
+"#  #  # ###### #       ####   ####  #    # ######    #####  ####     #     #   # #   ##   ##   # #        #  #     # ##    #     ###\n"+ 
+"#  #  # #      #      #    # #    # ##  ## #           #   #    #    #     #  #   #  # # # #  #   #      #   #     # # #   #     ###\n"+
+"#  #  # #####  #      #      #    # # ## # #####       #   #    #    ######  #     # #  #  # #     #    #    #     # #  #  #      #\n"+ 
+"#  #  # #      #      #      #    # #    # #           #   #    #    #     # ####### #     # #######   #     #     # #   # #     \n"+
+"#  #  # #      #      #    # #    # #    # #           #   #    #    #     # #     # #     # #     #  #      #     # #    ##     ###\n"+
+" ## ##  ###### ######  ####   ####  #    # ######      #    ####     ######  #     # #     # #     # ####### ####### #     #     ###\n"
+    )
+    console.log("\t\tYou are now connected to Bamazon.\n\t\tWhile our website is being reconstructed, you can shop here.\n\t\tTake your time to browse the table.\n\t\tIf you wish to purchase something, select the id number and follow the prompts to checkout.\n\t\t We are having a big summer sales event so stock up for next year!\n\n")
     enterBamazon();
 });
 
@@ -20,7 +29,7 @@ function enterBamazon () {
     connection.query ("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         for (var i=0; i< res.length; i ++){
-            console.log(" Item: "+ res[i].product_name)
+            console.log( res[i].product_name)
         }
         connection.end();
     });
